aa510f6e3913f073454c46581c8f9cb35f556a62
Shuming Fang <fangshuming519@gmail.com>
2017-12-06 22:25:44 +0800

    Create A Very Big Sum.R

---
+++ A Very Big Sum.R
@@ -0,0 +1,10 @@
+#December 2017 by Shuming Fang. 
+#fangshuming519@gmail.com.
+#https://github.com/FonzieTree
+#https://www.hackerrank.com/challenges/a-very-big-sum/problem
+options(scipen=200)
+con=file('stdin',open='r')
+open(con)
+data=readLines(con,warn=F)
+data=sum(as.numeric(strsplit(data[2],split=' ')[[1]]))
+cat(data,'\n')
